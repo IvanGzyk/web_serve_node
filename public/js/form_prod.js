d3da06f7085e37e883a2972ec9c3f572b351179b
Arno <sistema@engepecas.com.br>
2021-12-28 15:12:41 -0300

    tt

--- public/js/form_prod.js
+++ public/js/form_prod.js
@@ -11,8 +11,15 @@ $('#ipv').on('change', function (event) {
     let custo = document.getElementById('custo').value
     let ipv = document.getElementById('ipv').value
     let preco = document.getElementById('preco')
-    soma = custo * ipv
-    preco.value = soma.toFixed(2)
+    if (ipv < 1.5) {
+        alert('IPV minimo 1.5')
+        soma = custo * 1.5
+        preco.value = soma.toFixed(2)
+        document.getElementById('ipv').value = 1.5
+    } else {
+        soma = custo * ipv
+        preco.value = soma.toFixed(2)
+    }
 })
 
 $('#estoque').on('change', function (event) {
