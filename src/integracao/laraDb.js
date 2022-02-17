f00d2f4965697f45d6fc93a0070b71b5af1cf338
Arno <sistema@engepecas.com.br>
2022-02-17 17:19:19 -0300

    cadastro de Clientes e endereço

--- src/integracao/laraDb.js
+++ src/integracao/laraDb.js
@@ -2,7 +2,7 @@ const laraDb = require('../../config/LaraDb')
 lara = new laraDb()
 const params = lara.GeraParamsUrl()
 const ApiLara = lara.ApiLaravel()
- 
+
 function retornaData(data) {
     data = data.data
     data = data.data
@@ -37,6 +37,24 @@ async function getCountries(params) {
     }
 }
 
+async function postPartners(params) {
+    try {
+        const response = await ApiLara.post(`/entriesBusinessPartners`, params)
+        return response
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+async function postPartnersAddresses(params) {
+    try {
+        const response = await ApiLara.post(`/entriesBusinessPartnersAddresses`, params)
+        return response
+    } catch (error) {
+        console.error(error)
+    }
+}
+
 /** EXEMPLOS */
 
 /** pega Pais onde name for igual a Brasil */
@@ -58,3 +76,34 @@ async function getCountries(params) {
 
 /** Pega Endereços */
 //getPartnersAdsresses(params).then(console.log)
+
+/** Cadastra novo Cliente */
+
+var jsonCliente = {
+    "company_name": "Teste2",
+    "cpf": "13345876951",
+    "birth_date": "2022-02-17",
+    "email": "teste2@teste2.com"
+}
+
+postPartners(jsonCliente).then(data => {
+    data = data.data.data
+    idCliente = data.id
+    var search = encodeURI('name=Curitiba')
+    var relations = encodeURI('Estado')
+    getCities(`?search=${search}&relations=${relations}`).then(data => {
+        let dados = retornaData(data)
+        dados = dados[0]
+        city_id = dados.id
+        state_id = dados.state_id
+        var jsonEndereco = {
+            "entries_business_partner_id": idCliente,
+            "zip": "8457891",
+            "address": "R. Teste, n. 1000",
+            "city_id": city_id,
+            "state_id": state_id,
+            "country_id": 1
+        }
+        postPartnersAddresses(jsonEndereco).then(console.log)
+    })
+})
