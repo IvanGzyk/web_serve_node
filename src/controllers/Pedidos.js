2fd832dc3306e21dbea5c4d27b7060f65b8b6131
Arno <sistema@engepecas.com.br>
2022-02-22 10:27:21 -0300

    Reinstruturado Pedidos para o Padrão

--- src/controllers/Pedidos.js
+++ src/controllers/Pedidos.js
@@ -1,4 +1,9 @@
 const itemsModel = require("../models/items")
+const IntegracaoPedidos = require('../integracao/pedidos')
+const Orders = require("../models/orders")
+const Adress = require("../models/address")
+const Items = require("../models/items")
+const Payment = require("../models/payment")
 class Pedidos {
     constructor() {
 
@@ -33,6 +38,93 @@ class Pedidos {
             res.render("home/home")
         }
 
+    }
+
+    getDados(dados) {
+        const Pedidos = dados['items']
+        if (Pedidos.length > 0) {
+            Pedidos.forEach(element => {
+    
+                IntegracaoPedidos.getPedidos(element.entity_id).then(dados => {
+                    if (dados.length > 0) {
+                        dados.forEach(array => {
+                            Orders.updateOreder(element, array.dataValues.id)
+                        })
+                    } else {
+                        Orders.createOreder(element)
+                    }
+                })
+    
+                const items = element.items
+                items.forEach(item => {
+                    IntegracaoPedidos.getItem(item.order_id).then(dados => {
+                        if (dados.length > 0) {
+                            dados.forEach(array => {
+                                Items.updateItems(item, array.id)
+                            })
+                        } else {
+                            dados.forEach(array => {
+                                Items.createItems(item)
+                            })
+                        }
+                    })
+                })
+    
+                const End = element.billing_address
+                IntegracaoPedidos.getAdress(End.entity_id).then(dados => {
+                    if (dados.length != 0) {
+                        dados.forEach(array => {
+                            Adress.updateAddress(End, dados.entity_id)
+                        })
+                    } else if (dados.length == 0) {
+                        dados.forEach(array => {
+                            Adress.createAddress(End)
+                        })
+                    }
+                })
+            })
+        }
+    
+        let x = 0;
+        while (dados['total_count'] > x) {
+            const Address = dados['items'][x].billing_address
+            const Item = dados['items'][x].items[0]
+            const Pagamento = dados['items'][x].payment
+    
+            IntegracaoPedidos.getItem(Item.order_id).then(valor => roda(valor, 'item'))
+            IntegracaoPedidos.getAdress(Address.entity_id).then(valor => roda(valor, 'end'))
+            IntegracaoPedidos.getPag(Pagamento.entity_id).then(valor => roda(valor, 'pag'))
+    
+            function roda(value, tab) {
+    
+                if (value.length != 0) {
+                    switch (tab) {
+                        case 'item':
+                            const id_item = value[0].dataValues
+                            Items.updateItems(Item, id_item.id)
+                            break
+                        case 'end':
+                            const id_end = value[0].dataValues
+                            if (id_end.id != undefined) {
+                                Adress.updateAddress(Address, id_end.id)
+                            }
+                            break
+                        case 'pag':
+                            const id_pag = value[0].dataValues
+                            Payment.updatePayment(Pagamento, id_pag.id)
+                            break
+                    }
+                } else {
+                    if (Address.id != undefined) {
+                        Adress.createAddress(Address)
+                    }
+                    Items.createItems(Item)
+                    Payment.createPayment(Pagamento)
+                }
+            }
+            x++
+        }
+        return dados
     }
 }
 
--- src/cron/crontab.js
+++ src/cron/crontab.js
@@ -1,9 +1,15 @@
 const funcoes = require('../util/Util').Util
 const CronJob = require('cron').CronJob
+
 const ModelsCustomers = require('../models/customers')
 const IntegracaoCustomers = require('../integracao/customers')
 const ControllersCustomers = require('../controllers/Customers')
 var customers = ControllersCustomers.Customers
+
+const ModelsPedidos = require('../models/orders')
+const IntegracaoPedidos = require('../integracao/pedidos')
+const ControllersPedidos = require('../controllers/Pedidos')
+var pedidos = ControllersPedidos.Pedidos
 
 
 /** Verifica novos clientes a cada 5 minutos */
@@ -31,4 +37,25 @@ const job2 = new CronJob('10 */1 * * * *', () => {
     ModelsCustomers.getCustomers(data_busca).then(dados => {
         customers.SalvaClienteLaraApi(dados)
     })
+}, null, true, 'America/Sao_Paulo')
+
+
+/** Verifica as ordems a cada 5 minutos*/
+const job3 = new CronJob('*/20 * * * * *', () => {
+    data_atual = funcoes.dataAtual()/** hora atual */
+    data_inicio = funcoes.dataDeInicio(-5) /** Hora atual menos 5 minutos */
+
+    let params = {
+        $from: data_inicio,
+        $to: data_atual,
+        $sort: {
+            "created_at": "desc"
+        },
+        $perPage: 200,
+        $page: 1
+    }
+
+    IntegracaoPedidos.getOrders(params).then(data => data.data).then(dados => pedidos.getDados(dados))
+    console.log('Pedidos atualizado em: ' + data_atual)
+
 }, null, true, 'America/Sao_Paulo')
